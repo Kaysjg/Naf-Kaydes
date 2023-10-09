49465a39b0b11aed4846010bdd66120e57aef744
Kaysjg <fik5921us430@gmail.com>
2023-10-09 19:49:22 +0300

    Preddecision

--- functions_for_titanik.py
+++ functions_for_titanik.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def inpute_data(df):
-    '''Inputing NaN data'''
+    '''Inputing NaN data.'''
     df_copy = df.copy() #Copy so as not to change the original
     
     #Inputing NaN categorical values with column mode
@@ -24,8 +24,20 @@ def inpute_data(df):
     return df_copy
 
 
+def encode_categorical(df_train, df_test):
+    '''One-hot-encoding categorical data.'''
+    
+    test_start_ind = df_train.index[-1] #We remembered after which index the test sample will start
+    contated_df = pd.concat([df_train, df_test]) #Combining training and tesr samples
+    
+    encoded_df = pd.get_dummies(contated_df, drop_first=True) #Transformation
+    df_train_encoded, df_test_encoted = encoded_df.iloc[:test_start_ind], encoded_df.iloc[test_start_ind:] #Disconnection
+
+    return df_train_encoded, df_test_encoted
+
+
 def make_prediction_file(prediction, test_data, name):
-    '''Make_prediction_file'''
+    '''Make prediction file.'''
 
     result = pd.DataFrame({"PassengerId": test_data.reset_index()["PassengerId"], "Survived": prediction})
     return result.to_csv(name, index=False)
